7fa7f6cf1634b747134ebc8b9becf3a47f013391
PierreVir <pierre.virama@gmail.com>
2024-03-26 19:11:45 +0100

    quete 2 finished manipulate information with react/js

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,29 @@
 function PokemonCard(){
+
+    const pokemon = pokemonList[0]
+    console.log(pokemon);
+    // const icon = pokemon.imgSrc
+
     return (
 
-    <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
-    <figcaption>Bulbasaur</figcaption></figure>
+    <figure><img src={pokemon === pokemonList[0] ? pokemon.imgSrc : "???"} alt={pokemon.name}/>
+    <figcaption>{pokemon.name}</figcaption></figure>
+    
 )
+
+
+
+
 }
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name:"mew",
+    }
+];
 
 export default PokemonCard;
